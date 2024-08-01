70b5e3bb06bf931d926214c75a34142efc6e4dcd
Omar Abdullah <omarmoharam790@gmail.com>
2024-08-01 20:34:43 +0300

    Overflow Fixed

--- Flutter/Week 0/flutter_day_4/quiz_app_task/lib/Screens/welcome.dart
+++ Flutter/Week 0/flutter_day_4/quiz_app_task/lib/Screens/welcome.dart
@@ -16,134 +16,123 @@ class WelcomeScreen extends StatelessWidget {
       key: formKey,
       child: Scaffold(
         backgroundColor: const Color(0xFF86948F),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 80),
-              Text(
-                'QUIZ',
-                style: GoogleFonts.kleeOne(
-                  fontSize: 50,
-                  fontWeight: FontWeight.w600,
-                  height: 1.5,
-                  color: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 80),
+                Text(
+                  'QUIZ',
+                  style: GoogleFonts.kleeOne(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600,
+                    height: 1.5,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const CircleAvatar(
-                radius: 120,
-                backgroundColor: Colors.white,
-                child: CustomImage(),
-              ),
-              const SizedBox(height: 20),
-              const Spacer(
-                flex: 1,
-              ),
-              Stack(
+                const SizedBox(height: 20),
+                const CircleAvatar(
+                  radius: 120,
+                  backgroundColor: Colors.white,
+                  child: CustomImage(),
+                ),
+                const SizedBox(height: 20),
+                const SizedBox(height: 40),
+                Stack(
                   clipBehavior: Clip.none,
                   alignment: Alignment.center,
                   children: [
                     ClipRRect(
                       borderRadius: BorderRadius.circular(10),
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 350,
-                            height: 200,
-                            color: const Color(0xffABBFB8),
-                            child: Column(
-                              children: [
-                                Container(
-                                  alignment: Alignment.topCenter,
-                                  width: 400,
-                                  height: 29,
-                                  margin: const EdgeInsets.only(
-                                    top: 40,
-                                    bottom: 15,
+                      child: Container(
+                        width: 350,
+                        color: const Color(0xffABBFB8),
+                        child: Column(
+                          children: [
+                            Container(
+                              alignment: Alignment.topCenter,
+                              width: double.infinity,
+                              padding:
+                                  const EdgeInsets.only(top: 40, bottom: 15),
+                              child: const Text(
+                                'Please Enter Your Name ',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 232,
+                              height: 30,
+                              child: TextFormField(
+                                textAlign: TextAlign.center,
+                                controller: nameController,
+                                validator: (value) => value!.isEmpty
+                                    ? 'Please Enter Your Name'
+                                    : null,
+                                decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10.0)),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                      width: 2,
+                                    ),
                                   ),
-                                  child: const Text(
-                                    'Please Enter Your Name ',
-                                    style: TextStyle(
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 20, bottom: 20),
+                              child: SizedBox(
+                                height: 29,
+                                width: 108,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: const Color(0xffB8E893),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      side: const BorderSide(
+                                        color: Color(0xffB8E893),
+                                        width: 1,
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    if (formKey.currentState!.validate()) {
+                                      name = nameController.text;
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              const QuestionScreen(),
+                                        ),
+                                      );
+                                    }
+                                  },
+                                  child: Text(
+                                    'SUBMIT',
+                                    style: GoogleFonts.inter(
                                       color: Colors.black,
                                       fontSize: 14,
                                       fontWeight: FontWeight.w700,
                                     ),
-                                    textAlign: TextAlign.center,
                                   ),
                                 ),
-                                //textFeild here🔻🔻
-                                SizedBox(
-                                  width: 232,
-                                  height: 26,
-                                  child: TextFormField(
-                                    textAlign: TextAlign.center,
-                                    controller: nameController,
-                                    validator: (value) => value!.isEmpty
-                                        ? 'Please Enter Your Name'
-                                        : null,
-                                    decoration: const InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.white,
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(10.0)),
-                                        borderSide: BorderSide(
-                                          color: Colors.white,
-                                          width: 2,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                //button here🔻 🔻
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 20),
-                                  child: SizedBox(
-                                    height: 29,
-                                    width: 108,
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        foregroundColor: Colors.white,
-                                        backgroundColor:
-                                            const Color(0xffB8E893),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                          side: const BorderSide(
-                                            color: Color(0xffB8E893),
-                                            width: 1,
-                                          ),
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        if (formKey.currentState!.validate()) {
-                                          name = nameController.text;
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const QuestionScreen(),
-                                            ),
-                                          );
-                                        }
-                                      },
-                                      child: Text(
-                                        'SUBMIT',
-                                        style: GoogleFonts.inter(
-                                          color: Colors.black,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                     Positioned(
@@ -170,11 +159,11 @@ class WelcomeScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                  ]),
-              const Spacer(
-                flex: 2,
-              ),
-            ],
+                  ],
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
           ),
         ),
       ),
