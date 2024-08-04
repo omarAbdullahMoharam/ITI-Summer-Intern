d7853dc9bd0dd43af76a4f67fcf91ac11aa8df0c
Omar Abdullah <omarmoharam790@gmail.com>
2024-08-04 06:17:46 +0300

    Some Customization added

--- Flutter/Week  1/Flutter_day_1/fickle_flight/lib/screens/explore.dart
+++ Flutter/Week  1/Flutter_day_1/fickle_flight/lib/screens/explore.dart
@@ -3,6 +3,7 @@ import 'package:fickle_flight/components/custom_card.dart';
 import 'package:fickle_flight/components/custom_credit.dart';
 import 'package:fickle_flight/components/custom_icon.dart';
 import 'package:fickle_flight/components/custom_text.dart';
+import 'package:fickle_flight/components/plan_path.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -253,81 +254,5 @@ Widget customImage(Image img) {
     children: [
       img,
     ],
-  );
-}
-
-class PlanPath extends StatelessWidget {
-  const PlanPath({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Row(
-        children: [
-          _buildCircle(),
-          _buildDottedLine(),
-          _buildFlightIcon(),
-          _buildDottedLine(),
-          _buildCircle(),
-        ],
-      ),
-    );
-  }
-}
-
-Widget _buildCircle() {
-  return Container(
-    width: 20,
-    height: 20,
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      border: Border.all(
-        color: Colors.blue,
-        width: 2,
-      ),
-    ),
-    child: Center(
-      child: Container(
-        width: 10,
-        height: 10,
-        decoration: const BoxDecoration(
-          color: Colors.blue,
-          shape: BoxShape.circle,
-        ),
-      ),
-    ),
-  );
-}
-
-Widget _buildDottedLine() {
-  return DottedBorder(
-    color: Colors.grey,
-    strokeWidth: 1,
-    dashPattern: const [4, 4],
-    child: const SizedBox(
-      height: 1,
-      width: 40,
-    ),
-  );
-}
-
-Widget _buildFlightIcon() {
-  return Container(
-    width: 40,
-    height: 40,
-    decoration: BoxDecoration(
-      color: const Color(0xFF1262AE),
-      shape: BoxShape.circle,
-      border: Border.all(
-        color: Colors.blue,
-        width: 4,
-      ),
-    ),
-    child: const Center(
-      child: Icon(
-        CupertinoIcons.airplane,
-        color: Colors.white,
-      ),
-    ),
   );
 }
