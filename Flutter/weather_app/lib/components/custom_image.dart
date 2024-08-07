21856fa16a65f142cff589aea2dd1137ba1d55cd
Omar Abdullah <omarmoharam790@gmail.com>
2024-08-07 03:58:17 +0300

    Start Screen Added

---
+++ Flutter/weather_app/lib/components/custom_image.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomImage extends StatelessWidget {
+  const CustomImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/components/WeatherLogo.png',
+      alignment: Alignment.center,
+      width: 293,
+      height: 210,
+    );
+  }
+}
---
+++ Flutter/weather_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/start_screen.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    );
+  }
+}
